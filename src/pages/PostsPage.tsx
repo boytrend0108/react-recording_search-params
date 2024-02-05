9fce1ec56fbb6875a902b0ebe1455a1ca7c24495
boytrend0108 <boytrend@gmail.com>
2024-02-05 11:21:56 +0200

    add filter

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,13 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import React, { useContext } from 'react';
 
 import { PostList } from '../components/PostList';
 import { PostsContext } from '../store/PostsContext';
 import { PostFilter } from '../components/PostFilter';
+import { Post } from '../types';
+
+function getFilteredPost(posts: Post[], search: URLSearchParams): Post[] {
+  const userId = +(search.get('userId') || 0);
+  const query = search.get('query') || '';
+  const letters = search.getAll('letters');
+
+  return posts.filter(post => {
+    const matchUserId = userId ? post.userId === userId : true;
+    const matchQuery = post.title.includes(query);
+    const matchLetters = letters.length === 0 || letters.some(letter => post.body.includes(letter));
+
+    return matchUserId && matchQuery && matchLetters;
+  });
+};
 
 export const PostsPage: React.FC = () => {
   const { posts } = useContext(PostsContext);
-  const visiblePosts = posts;
+  const [searchParams] = useSearchParams();
+
+  const visiblePosts = getFilteredPost(posts, searchParams);
 
   return (
     <div>
